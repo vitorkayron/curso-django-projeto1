8563302d6da1b7f03af5a4ce47a272b5cc3c3116
“Vitor <vitork2016@gmail.com>
2022-12-22 16:38:57 -0300

    removev contato and sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,6 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes/home.html', context={'name': 'Vitor', })
 
-
-def contato(request):
-    return HttpResponse('contato')
-
-
-def sobre(request):
-    return HttpResponse('sobre')
