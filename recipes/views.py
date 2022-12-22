4066eccb5a796698801edb1c4ceeaf61f577de83
“Vitor <vitork2016@gmail.com>
2022-12-22 16:09:39 -0300

    add templates

--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,9 @@
 from django.http import HttpResponse
-
-# from django.shortcuts import render
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse('HOME')
+    return render(request, 'recipes/home.html')
 
 
 def contato(request):
